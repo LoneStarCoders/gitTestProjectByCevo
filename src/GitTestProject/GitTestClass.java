1252b4147205df117fa7b3dae269278acef13fe4
cevdetabbas <cevdetabbas@gmail.com>
2020-05-09 21:19:38 -0500

    commit2

---
+++ src/GitTestProject/GitTestClass.java
@@ -0,0 +1,25 @@
+package GitTestProject;
+
+public class GitTestClass
+{
+    public static void main(String[] args) {
+        System.out.println("this is the first test line for git ");
+        //  first download git then
+        //  introduce yourself to git with
+        //  git config --global user.name "........."
+        //  git config --global user.email "....."
+        //  git config --global user.password "....."
+        //  git config --global color.ui true  // not neccessary
+        //  config --list
+
+        //  then you create your local repository on your computer
+        //  git init    // this is kind of starting service
+        //  git add .   //  this is adding your current directory to staging area for snapshot
+        //  git commit -m "..."  // this is commiting the file
+
+        //  now go to github.com and clic on + to create a new repository with the same name of your Project
+        //  push your already created repository to github with these commands
+        //  git remote add origin https://github.com/LoneStarCoders/YourProjectName.git
+        //  git push -u origin master
+    }
+}
